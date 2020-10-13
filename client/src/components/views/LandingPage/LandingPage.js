2e76f4e4d69a2e0607754af442977bfd8d590b61
Jinyoung Ahn <48481448+zeroaan@users.noreply.github.com>
2020-10-13 21:33:29 +0900

    logout

--- client/src/components/views/LandingPage/LandingPage.js
+++ client/src/components/views/LandingPage/LandingPage.js
@@ -1,10 +1,20 @@
 import React, { useEffect } from "react";
 import axios from "axios";
 
-const LandingPage = () => {
+const LandingPage = (props) => {
   useEffect(() => {
     axios.get("/api/hello").then((response) => console.log(response));
   }, []);
+
+  const onClick = () => {
+    axios.get("/api/users/logout").then((response) => {
+      if (response.data.success) {
+        props.history.push("/login");
+      } else {
+        alert("로그아웃을 하지 못했습니다.");
+      }
+    });
+  };
 
   return (
     <>
@@ -18,6 +28,7 @@ const LandingPage = () => {
         }}
       >
         <h2>시작 페이지</h2>
+        <button onClick={onClick}>로그아웃</button>
       </div>
     </>
   );
