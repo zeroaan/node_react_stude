7ec2dadf180e8205bc597df9aaaf946d5e992fc7
Jinyoung Ahn <48481448+zeroaan@users.noreply.github.com>
2020-10-13 10:38:29 +0900

    auth 오타 수정, logout

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,4 +1,4 @@
-const User = require("../models/User");
+const { User } = require("../models/User");
 
 let auth = (req, res, next) => {
   // 인증 처리 하는 곳
@@ -6,7 +6,7 @@ let auth = (req, res, next) => {
   let token = req.cookies.x_auth;
 
   // 토큰을 복호화 한 후 유저를 찾는다.
-  User.fineByToken(token, (err, user) => {
+  User.findByToken(token, (err, user) => {
     if (err) throw err;
     if (!user) return res.json({ isAuth: false, error: true });
 
