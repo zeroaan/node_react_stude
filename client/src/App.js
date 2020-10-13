27a6d66527d3ad9cbef3444d83fdd2c03a6b633e
Jinyoung Ahn <48481448+zeroaan@users.noreply.github.com>
2020-10-13 22:00:42 +0900

    auth

--- client/src/App.js
+++ client/src/App.js
@@ -3,15 +3,16 @@ import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import LandingPage from "./components/views/LandingPage/LandingPage";
 import LoginPage from "./components/views/LoginPage/LoginPage";
 import RegisterPage from "./components/views/RegisterPage/RegisterPage";
+import Auth from "./hoc/auth";
 
 function App() {
   return (
     <>
       <Router>
         <Switch>
-          <Route exact path="/" component={LandingPage} />
-          <Route exact path="/login" component={LoginPage} />
-          <Route exact path="/register" component={RegisterPage} />
+          <Route exact path="/" component={Auth(LandingPage, null)} />
+          <Route exact path="/login" component={Auth(LoginPage, false)} />
+          <Route exact path="/register" component={Auth(RegisterPage, false)} />
           <Route path="/">
             <div>Not Found...</div>
           </Route>
