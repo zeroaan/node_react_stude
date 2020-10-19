a60f1d997701869b02a5f57204c1d696657ba34f
Jinyoung Ahn <48481448+zeroaan@users.noreply.github.com>
2020-10-19 16:15:24 +0900

    server

--- mongoDB_test/server/index.js
+++ mongoDB_test/server/index.js
@@ -1,12 +1,15 @@
 const express = require("express");
 const app = express();
 const bodyParser = require("body-parser");
+const cookieParser = require("cookie-parser");
 const config = require("./config/key");
 
+const { auth } = require("./middleware/auth");
 const { User } = require("./models/User");
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
+app.use(cookieParser());
 
 const mongoose = require("mongoose");
 mongoose
@@ -32,6 +35,67 @@ app.post("/api/users/register", (req, res) => {
       success: true,
     });
   });
+});
+
+app.post("/api/users/login", (req, res) => {
+  // 요청한 데이터가 DB에 있는지 확인
+  User.findOne({ email: req.body.email }, (err, userInfo) => {
+    // 해당 email에 맞는 user가 없는 경우
+    if (!userInfo) {
+      return res.json({
+        loginSuccess: false,
+        message: "해당 이메일은 등록되지 않았습니다.",
+      });
+    }
+
+    // 해당 email이 DB에 있다면 비밀번호 확인
+    userInfo.comparePassword(req.body.password, (err, isMatch) => {
+      if (!isMatch) {
+        return res.json({
+          loginSuccess: false,
+          message: "비밀번호가 틀렸습니다.",
+        });
+      }
+
+      // 비밀번호까지 맞다면 토큰을 생성
+      userInfo.generateToken((err, userInfo) => {
+        if (err) {
+          return res.status(400).send(err);
+        }
+
+        // 토큰을 저장한다.
+        res
+          .cookie("userInfo", userInfo.token)
+          .status(200)
+          .json({ loginSuccess: true, userID: userInfo._id });
+      });
+    });
+  });
+});
+
+app.get("/api/users/auth", auth, (req, res) => {
+  // 여기 까지 미들웨어를 해왔다는 얘기는 Authentication 이 True라는 말
+  res.status(200).json({
+    _id: req.user._id,
+    name: req.user.name,
+    email: req.user.email,
+    isAuth: true,
+  });
+});
+
+app.get("/api/users/logout", auth, (req, res) => {
+  User.findOneAndUpdate(
+    { _id: req.user._id },
+    { token: "" },
+    (err, userInfo) => {
+      if (err) {
+        return res.json({ success: false, err });
+      }
+      return res.status(200).send({
+        success: true,
+      });
+    }
+  );
 });
 
 const port = 3001;
--- mongoDB_test/server/models/User.js
+++ mongoDB_test/server/models/User.js
@@ -1,4 +1,7 @@
 const mongoose = require("mongoose");
+const bcrypt = require("bcrypt");
+const saltRounds = 10;
+const jwt = require("jsonwebtoken");
 
 const userSchema = mongoose.Schema({
   name: {
@@ -16,7 +19,71 @@ const userSchema = mongoose.Schema({
     required: true,
     minlength: 5,
   },
+  token: {
+    type: String,
+  },
 });
+
+userSchema.pre("save", function (next) {
+  const user = this;
+
+  if (user.isModified("password")) {
+    // 비밀번호 암호화
+    bcrypt.genSalt(saltRounds, (err, salt) => {
+      if (err) {
+        return next(err);
+      }
+      bcrypt.hash(user.password, salt, (err, hash) => {
+        if (err) {
+          return next(err);
+        }
+        user.password = hash;
+        next();
+      });
+    });
+  } else {
+    next();
+  }
+});
+
+userSchema.methods.comparePassword = function (plainPassword, cb) {
+  const user = this;
+  bcrypt.compare(plainPassword, user.password, function (err, isMatch) {
+    if (err) {
+      return cb(err);
+    }
+    cb(null, isMatch);
+  });
+};
+
+userSchema.methods.generateToken = function (cb) {
+  const user = this;
+  const token = jwt.sign(user._id.toHexString(), "secretToken");
+
+  user.token = token;
+  user.save(function (err, user) {
+    if (err) {
+      return cb(err);
+    }
+    cb(null, user);
+  });
+};
+
+userSchema.statics.findByToken = function (token, cb) {
+  const user = this;
+
+  // 토큰을 decode 한다.
+  jwt.verify(token, "secretToken", function (err, decoded) {
+    // 유저 아이디를 이용해서 유저를 찾은 다음에
+    // 클라이언트에서 가져온 token과 DB에 보관된 토큰이 일치하는지 확인
+    user.findOne({ _id: decoded, token: token }, function (err, user) {
+      if (err) {
+        return cb(err);
+      }
+      cb(null, user);
+    });
+  });
+};
 
 const User = mongoose.model("User", userSchema);
 
